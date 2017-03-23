f6b3fbfb57d7cccfd87dafed87202feb5c2ce921
COMPLAINTE\Elsa <e.becker@ludus-academie.com>
2017-03-23 17:56:35 +0100

    Ajout de l'application dans son dernier état d'avancement (connexion + ajout utilisateur)

---
+++ BDD_Bachelor_BeckerElsa_Application/app/src/main/java/com/example/elsa/bdd_bachelor_beckerelsa/CreateUser.java
@@ -0,0 +1,85 @@
+package com.example.elsa.bdd_bachelor_beckerelsa;
+
+/**
+ * Created by Elsa on 23/03/2017.
+ */
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.widget.Toast;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+
+public class CreateUser extends AsyncTask<String, Void, String>{
+
+    private Context context;
+
+    public CreateUser(Context context){
+        this.context = context;
+    }
+
+    protected void onPreExecute(){
+
+    }
+
+
+    @Override
+    protected String doInBackground(String... arg0){
+        String log = arg0[0];
+        String mdp = arg0[1];
+
+        String lien;
+        String stock;
+        BufferedReader bufferedReader;
+        String result;
+
+        try{
+            stock = "?Login=" + URLEncoder.encode(log, "UTF-8");
+            stock += "&Password=" + URLEncoder.encode(mdp, "UTF-8");
+
+            lien = "http://10.0.2.2/Bdd_ngl/create_account.php" + stock;
+            URL url = new URL(lien);
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+
+            bufferedReader = new BufferedReader(new InputStreamReader(con.getInputStream()));
+            result = bufferedReader.readLine().toString();
+            return result;
+
+        }catch (Exception e){
+            return new String("Exception: " + e.getMessage());
+        }
+    }
+
+
+    @Override
+    protected void onPostExecute(String result){
+        String jsonString = result;
+        if (jsonString != null){
+            try{
+                JSONObject jsonObj = new JSONObject(jsonString);
+                String query_result = jsonObj.getString("query_result");
+                if (query_result.equals("SUCCESS")){
+                    Toast.makeText(context, "Connexion réussie", Toast.LENGTH_SHORT).show();
+                    //goToNextActivity();
+                }else if (query_result.equals("FAILURE")){
+                    Toast.makeText(context, "Connexion échouée", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(context, "Connexion à la base de données échouée", Toast.LENGTH_SHORT).show();
+                }
+            }catch (JSONException e){
+                e.printStackTrace();
+                Toast.makeText(context, "Erreur analyse données JSON.", Toast.LENGTH_SHORT).show();
+            }
+        }else{
+            Toast.makeText(context, "Données JSON introuvables.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+}
--- BDD_Bachelor_BeckerElsa_Application/app/src/main/java/com/example/elsa/bdd_bachelor_beckerelsa/user_connection.java
+++ BDD_Bachelor_BeckerElsa_Application/app/src/main/java/com/example/elsa/bdd_bachelor_beckerelsa/user_connection.java
@@ -1,13 +1,61 @@
 package com.example.elsa.bdd_bachelor_beckerelsa;
 
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+
 
 public class user_connection extends AppCompatActivity {
+
+    EditText login;
+    EditText password;
+    Button connect;
+    Button createaccount;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_connection);
+
+        login = (EditText) findViewById(R.id.edit_log);
+        password = (EditText) findViewById(R.id.edit_mdp);
+        connect = (Button) findViewById(R.id.button_connect);
+        createaccount = (Button) findViewById(R.id.button_newuser);
+
+        connect.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                String log = login.getText().toString();
+                String mdp = password.getText().toString();
+
+                Toast.makeText(user_connection.this, "Connexion...", Toast.LENGTH_SHORT).show();
+                new ConnectUser(user_connection.this).execute(log, mdp);
+                /*Toast.makeText(this, "Connexion...", Toast.LENGTH_SHORT).show();
+                new ConnectUser(this).execute(log, mdp);*/
+
+            }
+        });
+
+
+        createaccount.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                String log = login.getText().toString();
+                String mdp = password.getText().toString();
+
+                Toast.makeText(user_connection.this, "Création...", Toast.LENGTH_SHORT).show();
+                new CreateUser(user_connection.this).execute(log, mdp);
+
+            }
+        });
+
     }
+
 }
